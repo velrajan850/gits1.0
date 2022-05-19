5d1ad6607bf29ab691085cc8cb605c320b0679d1
velrajan850 <105338994+velrajan850@users.noreply.github.com>
2022-05-19 15:24:37 +0530

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,24 @@
+package demoo;
+
+public class loop1 {
+	 
+	    
+	        public static void main(String[] args) {
+	               for (int i = 1; i <= 7; i++) {
+	            	   
+	                for (int j = 1; j <= 7; j++) {
+	                	
+	                    if (j <= i) {
+	                            System.out.print(j);
+	                    }
+	                    else
+	                    {
+	                        System.out.print("*");
+	                    }
+	            }
+	                System.out.println("\n");
+	        }
+	        }
+	    } 
+
+
